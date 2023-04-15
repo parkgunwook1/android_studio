3af2929bf64dead1900d6ea92dbb41a2c0671e2f
parkgunwook <pkw85428@naver.com>
2023-04-15 15:29:16 +0900

    4.학습중

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,48 @@
 package com.example.myapplication;
+
 import androidx.appcompat.app.AppCompatActivity;
+// import 문장은 외부에서 패키지나 클래스를 포함시키는 것
+// 앞에 androidx가 붙은 패키지는 JetPack에 속하는 클래스로서 호환성을 위하여 최근에 사용이 권장되는 패키지이다.
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
+// Activity로부터 상속받았으므로 액티비티가 된다.
+// 액티비티는 안드로이드에서 애플리케이션을 구성하는 4가지의 컴포넌트 중의 하나이다.
+// 액티비티는 화면을 통하여 사용자와 상호작용하는 횔동을 의미한다.
 
-   public void onCreate(Bundle savedInstanceState) {
+    @Override // 메소드가 부모 클래스의 메소드를 재정의 하였다는 것을 나타낸다.
 
-       super.onCreate(savedInstanceState);
-           setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {
+        //onCreate() 메소드는 액티비티가 생성되는 순간에 딱 합번 호출
+        // 모든 초기화와 사용자 인터페이스 설정이 여기에 들어간다.
+        // 액티비티 중에서는 onCreate() 메소드가 가장 먼저 실행된다.
 
-           final Button button = (Button) findViewById(R.id.button);
+        super.onCreate(savedInstanceState); //부모 클래스인 AppCompatActivity 클래스의 onCreate()를 호출하는 문장.
 
-           button.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View view) {
-                   Toast.makeText(getApplicationContext(), "버튼이 눌러졌습니다." , Toast.LENGTH_SHORT).show();
-               }
-           });
-       }
+        setContentView(R.layout.activity_main);
+        //setContentView() 라는 함수는 액티비티의 화면을 설정하는 함수
+        // R.layout.activity_main은 activity_main.xml 파일을 나타낸다.
+    }
+
+    public void onclicked(View v) {
+        Toast.makeText(getApplicationContext(), "버튼이 눌러졌습니다.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    final Button button = (Button) findViewById(R.id.button4);
+
+    button.setOnClickListener(new View.OnClickListener()
+
+    {
+        public void onClick (View v){
+
+        Toast.makeText(getApplicationContext(), "버튼이 눌러졌습니다"
+                , Toast.LENGTH_SHORT).show();
+    }
+    });
 }
