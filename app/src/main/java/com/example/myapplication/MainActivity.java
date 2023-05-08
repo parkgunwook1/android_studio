d88c68af76b00b5b60a4205caa1a134af5c1ad42
parkgunwook <pkw85428@naver.com>
2023-05-08 21:03:18 +0900

    주사ã위

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,37 +4,131 @@ import androidx.appcompat.app.AppCompatActivity;
 // import 문장은 외부에서 패키지나 클래스를 포함시키는 것
 // 앞에 androidx가 붙은 패키지는 JetPack에 속하는 클래스로서 호환성을 위하여 최근에 사용이 권장되는 패키지이다.
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnadd,btnsub,btnmul,btndiv,btntemp;
+    TextView textresult;
+    String num1,num2;
+    Integer result;
 
-    private TimePicker timePicker1;
-    private TextView time;
-    private Calendar calendar;
-    private String format = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timePicker1 = (TimePicker)  findViewById(R.id.timePicker1);
+        setTitle("초간단계산기");
+
+        edit1 = (EditText) findViewById(R.id.editText1);
+        edit2 = (EditText) findViewById(R.id.editText2);
+        btnadd = (Button) findViewById(R.id.button1);
+        textresult = (TextView) findViewById(R.id.TextResult);
+
+        btnsub = (Button)findViewById(R.id.button2);
+        btnmul = (Button)findViewById(R.id.button3);
+        btndiv = (Button)findViewById(R.id.button4);
+        btntemp = (Button)findViewById(R.id.button5);
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(edit1.getText().toString().length() == 0 || edit2.getText().toString().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    textresult.setText("계산결과: none" );
+                }
+                else {
+                    result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    textresult.setText("계산결과: " + result.toString());
+                }
+            }
+        });
+
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if(edit1.getText().toString().length() == 0 || edit2.getText().toString().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    textresult.setText("계산결과: none" );
+                }
+                else {
+                    result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    textresult.setText("계산결과: " + result.toString());
+                }
+            }
+        });
+
+        btnmul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if(edit1.getText().toString().length() == 0 || edit2.getText().toString().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    textresult.setText("계산결과: none" );
+                }
+                else {
+                    result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    textresult.setText("계산결과: " + result.toString());
+                }
+            }
+        });
+
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if(edit1.getText().toString().length() == 0 || edit2.getText().toString().length() == 0 ||Integer.parseInt(num2)==0)
+                {
+                    Toast.makeText(getApplicationContext(), "계산할수없습니다.", Toast.LENGTH_SHORT).show();
+                    textresult.setText("계산결과: none" );
+                }
+                else {
+                    result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                    textresult.setText("계산결과: " + result.toString());
+                }
+            }
+        });
+
+        btntemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if(edit1.getText().toString().length() == 0 || edit2.getText().toString().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    textresult.setText("계산결과: none" );
+                }
+                else {
+                    result = Integer.parseInt(num1) % Integer.parseInt(num2);
+                    textresult.setText("계산결과: " + result.toString());
+                }
+            }
+        });
+
+
     }
-
-
-
 }
+
     // 리스너 객체를 이용하여 처리 방법
 
    /* class Listener implements  View.OnClickListener {
